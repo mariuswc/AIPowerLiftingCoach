b88612d48c5fd1fd22de12cd5ceaed7b3f607b09
Marius <mariuscw@outlook.com>
2026-07-24 22:03:08 +0200

    the predictor now works, need to work further on making the predictor retreive file from standardized path and not a static url

--- src/main/kotlin/service/DjlService.kt
+++ src/main/kotlin/service/DjlService.kt
@@ -19,7 +19,9 @@ import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
 import java.io.FileNotFoundException
+import java.nio.file.Path
 import java.nio.file.Paths
+import kotlin.io.path.readBytes
 
 
 @Service
@@ -28,7 +30,7 @@ class DjlService(
     @param:Value("\${image.path}")
     private val imagePath: String
 ) {
-  private val predictor: Predictor<Image, Array<Joints>> = zooModel.newPredictor()
+    private val predictor: Predictor<Image, Array<Joints>> = zooModel.newPredictor()
 
 
     init {
@@ -36,47 +38,47 @@ class DjlService(
     }
 
     fun validateImage(image: MultipartFile) {
-        requireNotNull(image){"Image was null"}
-            val imageExtension = FilenameUtils.getExtension(image.originalFilename?.lowercase())
-
-            if (imageExtension !in AllowedFileType.entries.map { it.name })
-                throw FileNotFoundException("Please enter a valid file, allowed filetypes are ${AllowedFileType.entries.map { it.name.lowercase() } }}")
-        }
-
-
-    fun analyzeJoints(image: MultipartFile): Predictor<Image?, Array<Joints>?>? {
-        val imagePath = Paths.get(imagePath)
-        val file: Image = ImageFactory.getInstance().fromFile(imagePath)
-        validateImage(image)
-        val predict = predictor.predict(file)
-        println(predict.contentToString())
-
+        requireNotNull(image) { "Image was null" }
+//            val imageExtension = FilenameUtils.getExtension(image.originalFilename?.lowercase())
+//
+//            if (imageExtension !in AllowedFileType.entries.map { it.name })
+//                throw FileNotFoundException("$imageExtension is not valid" +
+//                         "allowed filetypes are ${AllowedFileType.entries.map { it.name.lowercase() } }}")
+        uploadImageToDir(image)
     }
 
 
-        fun uploadImageToDir(image: MultipartFile) {
+    fun analyzeJoints(image: MultipartFile): Array<Joints> {
+        //validateImage(image)
+        val imagePath = Paths.get("images/Generated Image July 16, 2026 - 4_36PM.jpg")
+        val img: Image = ImageFactory.getInstance().fromFile(imagePath)
+        return predictor.predict(img)
 
-            val imageDir = File(imagePath)
-            val fileName = requireNotNull(image.originalFilename)
-            val imageFile = File(imageDir, fileName)
+    }
 
-            try {
-                if (!imageDir.exists()){
-                    imageDir.mkdirs()
-                    imageFile.writeBytes(image.bytes)
-                    println("Directory succesfully written: ${imageFile.path}")
-                }
-                else{
-                    imageFile.writeBytes(image.bytes)
-                    println("Image has been uploaded")
-                }
-            } catch (e: Exception) {
-                println("somethings happen, $e")
+    fun uploadImageToDir(image: MultipartFile) {
+
+        val imageDir = File(imagePath)
+        val fileName = requireNotNull(image.originalFilename)
+        val imageFile = File(imageDir, fileName)
+
+        try {
+            if (!imageDir.exists()) {
+                imageDir.mkdirs()
+                imageFile.writeBytes(image.bytes)
+                println("Directory succesfully written: ${imageFile.path}")
+            } else {
+                imageFile.writeBytes(image.bytes)
+                println("Image has been uploaded")
             }
+        } catch (e: Exception) {
+            println("somethings happen, $e")
         }
-
-    enum class AllowedFileType(mimetype: String){PNG("text/pdf"),JPG("text/pdf")}
+    }
 }
+//
+//    enum class AllowedFileType(){PNG,JPG}
+//}
 
 
 
